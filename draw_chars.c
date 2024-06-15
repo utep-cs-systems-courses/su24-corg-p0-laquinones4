56fc5070c5ab9f4debbf120b0e7db895c28bc71b
laquinones4 <laquinones4@miners.utep.edu>
2024-06-14 19:28:08 -0600

    letter rotation

--- draw_chars.c
+++ draw_chars.c
@@ -18,11 +18,11 @@ void print_char_5x7(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char col = 0; col < 12; col++) {
-    for (char row = 0; row < 7; row++) {
-      unsigned short rowBits = font_8x12[c][col];
-      unsigned short colMask = 1 << (7-row); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+  for (char col = 0; col < 8; col++) {
+    for (char row = 12; row > 0; row--) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (7-col);
+	putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
   }
